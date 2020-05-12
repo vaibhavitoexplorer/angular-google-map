f5ed43e62aac5a237c1ceaee0e4f743bce418f3d
vaibhavc <vaibhavachandole9224@gmail.com>
2020-05-12 13:35:56 +0530

    somewhere in the middle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDo-eO2xBQGXnhzxleugHz0xUZgEtYlt9E',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+       
